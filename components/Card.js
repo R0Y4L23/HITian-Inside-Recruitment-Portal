d6384d16e4c47bdda1a7f6dc7551f56e79a14649
R0Y4L23 <subhamroy102.hitece2020@gmail.com>
2022-05-09 23:43:16 +0530

    Initial Commit

---
+++ components/Card.js
@@ -0,0 +1,31 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import { Button, Card } from 'semantic-ui-react'
+import Image from 'next/image'
+
+const CardComponent = (props) => {
+    return (
+        <Card>
+            <Card.Content>
+                <Image
+                    height={100}
+                    width={100}
+                    src={props.image}
+                />
+                <Card.Header style={{ marginTop: 20 }}>{props.position}</Card.Header>
+                <Card.Description>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis quae consequuntur voluptatem accusantium recusandae ea?
+                </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <div className='ui two buttons'>
+                    <Button basic color='green'>
+                        Apply
+                    </Button>
+                </div>
+            </Card.Content>
+        </Card>
+    )
+}
+
+export default CardComponent
