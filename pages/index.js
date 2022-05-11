a6feea26b4987afd88685cef72e98a060a78cdbb
R0Y4L23 <subhamroy102.hitece2020@gmail.com>
2022-05-11 08:02:42 +0530

    Error Fixes For Build

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ const Index = () => {
             </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <iframe width="400" height="315" src="https://www.youtube.com/embed/r-gPNwKxRiw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe width="400" height="315" src="https://www.youtube.com/embed/r-gPNwKxRiw" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           </div>
         </div>
       </section>
